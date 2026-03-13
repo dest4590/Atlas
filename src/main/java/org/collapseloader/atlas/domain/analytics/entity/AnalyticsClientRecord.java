3e352480f4f71b1a3df1e8fc38cea0f2eb38c151
dest4590 <dest4590@gmail.com>
2026-03-13 21:24:38 +0200

    fix: make AnalyticsClientRecord as CASCADE

--- src/main/java/org/collapseloader/atlas/domain/analytics/entity/AnalyticsClientRecord.java
+++ src/main/java/org/collapseloader/atlas/domain/analytics/entity/AnalyticsClientRecord.java
@@ -2,15 +2,19 @@ package org.collapseloader.atlas.domain.analytics.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.collapseloader.atlas.domain.clients.entity.Client;
 
 /**
  * Analytics Client Record.
- * Contains information about a client launch event, including the client, launch timestamp, and platform.
+ * Contains information about a client launch event, including the client,
+ * launch timestamp, and platform.
  * Fields:
  * {@link #client} - The {@link Client} associated with the launch event.
  * {@link #launchTimestamp} - The timestamp of the client launch event.
- * {@link #platform} - The {@link Platform} platform on which the client was launched.
+ * {@link #platform} - The {@link Platform} platform on which the client was
+ * launched.
  */
 @Entity
 @Table(name = "analytics_clients", indexes = {
@@ -24,6 +28,7 @@ public class AnalyticsClientRecord {
 
     @ManyToOne
     @JoinColumn(name = "client_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Client client;
 
     @Column(name = "launch_timestamp", nullable = false)
