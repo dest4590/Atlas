887b692c2d064632207918cef44d183f40ff8154
dest4590 <dest4590@gmail.com>
2026-03-16 13:31:29 +0200

    fix: added auth verify for websockets admin endpoints

--- src/main/java/org/collapseloader/atlas/domain/admin/controller/AdminBroadcastController.java
+++ src/main/java/org/collapseloader/atlas/domain/admin/controller/AdminBroadcastController.java
@@ -4,10 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.collapseloader.atlas.domain.admin.dto.request.BroadcastRequest;
+import org.collapseloader.atlas.domain.users.entity.User;
 import org.collapseloader.atlas.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,8 @@ public class AdminBroadcastController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ApiResponse<Void>> broadcastMessage(@Valid @RequestBody BroadcastRequest request) {
-        log.info("Admin broadcasting message: {}", request);
+    public ResponseEntity<ApiResponse<Void>> broadcastMessage(@AuthenticationPrincipal User user, @Valid @RequestBody BroadcastRequest request) {
+        log.info("Admin broadcasting message: {}, admin: {}, id: {}", request, user.getUsername(), user.getId());
 
         if ("users".equalsIgnoreCase(request.getTarget())) {
             messagingTemplate.convertAndSend("/topic/broadcast/users", request);
